0dc358bc028d086609b24c71b3479690290cb8fd
Cedric Rische <cedricrische@gmail.com>
2022-06-09 22:59:58 +0200

    Added external headerStyles Added logo Added default props

--- src/appHeader/AppHeader.jsx
+++ src/appHeader/AppHeader.jsx
@@ -5,33 +5,40 @@ import HeaderStyles from "./HeaderStyles";
 import {useHistory} from "react-router-dom";
 import {useTranslation} from "react-i18next";
 import {Logout} from "@mui/icons-material";
+import StarwitLogo from "../assets/img/logo-white.png";
 
 function AppHeader(props) {
-    const {menuItems, title, logo} = props;
-    const headerStyles = HeaderStyles();
+    const {menuItems, title, logo, styles} = props;
     const history = useHistory();
     const {t} = useTranslation();
 
     return (
         <>
-            <AppBar position="fixed" className={headerStyles.appBar}>
-                <Toolbar className={headerStyles.toolbar}>
-                    <img className={headerStyles.menuLogoImg} src={logo} alt={"Logo of " + title}/>
-                    <Typography className={headerStyles.menuTitle} variant="h2" noWrap>
+            <AppBar position="fixed" className={styles.appBar}>
+                <Toolbar className={styles.toolbar}>
+                    <img className={styles.menuLogoImg} src={logo} alt={"Logo of " + title}/>
+                    <Typography className={styles.menuTitle} variant="h2" noWrap>
                         {title}
                     </Typography>
-                    <div className={headerStyles.spacer}/>
+                    <div className={styles.spacer}/>
                     {menuItems.map(item => (
-                        <Button key={item.title} color="secondary" disableRipple className={headerStyles.linkButton}
+                        <Button key={item.title} color="secondary" disableRipple className={styles.linkButton}
                                 onClick={() => history.push(item.link)}>{t(item.title)}</Button>
                     ))}
-                    <IconButton color="secondary" disableRipple className={headerStyles.linkButton}
+                    <IconButton color="secondary" disableRipple className={styles.linkButton}
                                 onClick={() => history.push("/logout")}><Logout/></IconButton>
                 </Toolbar>
             </AppBar>
-            <div className={headerStyles.contentSpacer}/>
+            <div className={styles.contentSpacer}/>
         </>
     );
+}
+
+AppHeader.defaultProps = {
+    styles: HeaderStyles(),
+    title: "A LireJarp App",
+    logo: StarwitLogo,
+    menuItems: []
 }
 
 export default AppHeader;
