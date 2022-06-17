356b729afc853c41d9025d10d6c89c6bdd0267e3
Anett Hübner <anett_huebner@gmx.de>
2022-06-17 15:35:04 +0200

    corrected statement and confirmation dialog

--- src/confirmationDialog/ConfirmationDialog.jsx
+++ src/confirmationDialog/ConfirmationDialog.jsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React, {useState} from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogActions from "@mui/material/DialogActions";
